1953f000aabcd3d6441d8a0e7f6281ebe00a377b
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-04-10 13:59:25 +0700

    Update merge

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Sidebar from "../components/sidebar/Sidebar";
 import "./MainLayout.css";
+import Top_Navbar from "../components/top_navbar/Top_Navbar";
 
 const MainLayout = ({ children }) => {
     const [isCollapsed, setIsCollapsed] = useState(false);
@@ -9,6 +10,7 @@ const MainLayout = ({ children }) => {
         <div className="app-layout">
             <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
             <div className={`main-content-main-layout ${isCollapsed ? "collapsed" : "expanded"}`}>
+                <Top_Navbar/>
                 {children}
             </div>
         </div>
